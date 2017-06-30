ed592e47e4c940e78c202f7c1e6e95fe8ea0527b
Joyce Yang <jcyang@hmc.edu>
2017-06-30 15:36:33 -0400

    Made resume format and secured page

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .authorizeRequests().antMatchers("/", "/assets/**", "/bootstrap3/**").permitAll()
+                .authorizeRequests().antMatchers( "/assets/**", "/bootstrap3/**").permitAll()
                 .antMatchers("/gosearch").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
