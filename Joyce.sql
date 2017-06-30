2cac2384a87e1a21ff6ed25475d27c731751c90d
Joyce Yang <jcyang@hmc.edu>
2017-06-30 09:13:11 -0400

    Made tables for security

--- Joyce.sql
+++ Joyce.sql
@@ -3,5 +3,13 @@ drop table transaction;
 select * from record;
 drop table record;
 UPDATE `joyce`.`record` 
-SET `company`='JHU', `degree`='BS', `duty`='Assistant',`email`='j@gmail.com', `field`='Psychology', `proficiency`='Expert', `skill`='Java',`work_dur`='2012-2013', `work_title`='Worker', `year_grad`='2010' 
+SET first='John', middle= 'J', last='Smith', `company`='JHU', `degree`='BS', `duty`='Assistant',`email`='j@gmail.com', `field`='Psychology', `proficiency`='Expert', `skill`='Java',`work_dur`='2012-2013', `work_title`='Worker', `year_grad`='2010'
 WHERE `id`='1';
+use joyce;
+select * from role;
+select * from user;
+select * from user_roles;
+INSERT INTO `joyce`.`user` (`id`, `enabled`, `password`, `username`) VALUES ('1', TRUE, 'password1', 'Bob');
+INSERT INTO `joyce`.`role` (`id`, `role`) VALUES ('1', 'ROLE_USER');
+INSERT INTO `joyce`.`role` (`id`, `role`) VALUES ('2', 'ROLE_ADMIN');
+INSERT INTO `joyce`.`user_roles` (`user_id`, `role_id`) VALUES ('1', '2');
