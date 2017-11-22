9a431f5517f7a1d798df9650cea8b864252ddb2b
Joyce Yang <jcyang@hmc.edu>
2017-11-22 16:58:50 -0500

    First commit

--- src/main/java/com/example/demo/CWAOct2017WeeklyChallenge7.java
+++ src/main/java/com/example/demo/CWAOct2017WeeklyChallenge7.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeekSixChallengeApplication {
+public class CWAOct2017WeeklyChallenge7 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeekSixChallengeApplication.class, args);
+		SpringApplication.run(CWAOct2017WeeklyChallenge7.class, args);
 	}
 }
--- src/test/java/com/example/demo/CWAOct2017WeeklyChallenge7Tests.java
+++ src/test/java/com/example/demo/CWAOct2017WeeklyChallenge7Tests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WeekSixChallengeApplicationTests {
+public class CWAOct2017WeeklyChallenge7Tests {
 
 	@Test
 	public void contextLoads() {
